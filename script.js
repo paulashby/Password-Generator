dd6ebee846f8e89a2f96603533d0867d7b02d427
Paul Ashby <paul@primitive.co>
2022-11-22 14:53:14 +0000

    Accept text input for character set inclusion so cancel button can be used to actually cancel instead of to reject a character set. Check if Object hasOwn property in body of for in loop. Use switch statement to handle user input on character set. Make prompt for password length more concise.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var MIN_PW_LENGTH = 10;
+var MAX_PW_LENGTH = 64;
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -94,10 +97,7 @@ var characterTypeChoices = {
   uppercase: upperCasedCharacters,
   numeric: numericCharacters,
   special: specialCharacters
-}
-
-var MIN_PW_LENGTH = 10;
-var MAX_PW_LENGTH = 64;
+};
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -105,26 +105,43 @@ function getPasswordOptions() {
   var options = {
     pwLength: 0,
     characterTypes: []
-  }
+  };
   
-  options.pwLength = prompt("How long would you like your password to be? Must be between " + MIN_PW_LENGTH + " and " + MAX_PW_LENGTH + " characters.");
+  options.pwLength = prompt("Please enter a password length between " + MIN_PW_LENGTH + " and " + MAX_PW_LENGTH + " characters.");
+
+  if (options.pwLength === null) {
+    return;
+  }
 
   if (options.pwLength < MIN_PW_LENGTH || options.pwLength > MAX_PW_LENGTH) {
-    alert ("Password must be between " + MIN_PW_LENGTH + " and " + MAX_PW_LENGTH);
-    getPasswordOptions();
+    alert ("Password length must be at least " + MIN_PW_LENGTH + " and no more than " + MAX_PW_LENGTH + " characters.");
+    return getPasswordOptions();
   } 
 
   // Keep prompting for characterTypes if none have been chosen
   while (options.characterTypes.length < 1) {
-
     // Iterate through choices and prompt user to determine whether each should be included 
     for (var charType in characterTypeChoices) {
-        if (prompt("Include " + charType + " characters?") !== null) {
-        options.characterTypes.push(characterTypeChoices[charType]);
+      if (Object.hasOwn(characterTypeChoices, charType)) {
+        // Prompt user to accept or reject character set
+        var userinput = prompt("Include " + charType + " characters? Enter 'y' to accept or any other key to reject.");
+        
+        switch (userinput) {
+          case null:
+            // User cancelled the operation
+            return null;
+
+          case "y":
+            options.characterTypes.push(characterTypeChoices[charType]);
+            break;
+
+          default:
+            break;
+        }
       }
     }
     if (options.characterTypes.length === 0) {
-      alert("You must include at least one set of characters - please OK an option.")
+      alert("You must include at least one set of characters - please OK an option.");
     }
   }
   return options;
@@ -137,28 +154,39 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
   // Get user's preferences - length and character types
   var options = getPasswordOptions();
   var password = "";
-    
-  for (var i = 0; i < options.pwLength; i++) {
-
-    var charSet = getRandom(options.characterTypes);
-    password = password.concat(getRandom(charSet));
+  
+  if (options) {    
+    for (var i = 0; i < options.pwLength; i++) {
+      // Select a random character set
+      var charSet = getRandom(options.characterTypes);
+      // Select a random character and add to password
+      password = password.concat(getRandom(charSet));
+    }
+    return password;
   }
-  return password;
+  // User cancelled the operation
+  return null;
 }
 
-// Get references to the #generate element
+// Get reference to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  
+  // Build the password
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  if (password) {
+    // Get reference to the #password textarea
+    var passwordText = document.querySelector('#password');
+    
+    // Update the #password textarea to display the new password
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
